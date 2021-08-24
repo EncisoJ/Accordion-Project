b382baa99f1af34f330927895faa1b527d1100e2
Jason Enciso <Jencis275@west-mec.local>
2021-08-24 14:27:33 -0700

    all code

---
+++ 03-accordion/src/App.jsx
@@ -0,0 +1,13 @@
+import react from 'react'
+
+function App() {
+  return (
+    <div className="App">
+      <div className="mainCont">
+<h2>Questions And Answers About Login</h2>
+      </div>
+    </div>
+  );
+}
+
+export default App;
